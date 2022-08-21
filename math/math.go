a8d0803d5b933e726c07ebd5ac2e78c566c48a33
chen <test@test.com>
2022-08-21 17:35:52 +0800

    feat:四舍五入的新方法

--- math/math.go
+++ math/math.go
@@ -6,3 +6,11 @@ func MyRoundFloat(val float64, precision int8) float64 {
 	ratio := math.Pow10(int(precision))
 	return math.Round(val*ratio) / ratio
 }
+
+func MyRoundFloat2(f float64, digits int) float64 {
+	if math.Abs(f) < 0.5 {
+		return 0
+	}
+	pow := math.Pow10(digits)
+	return math.Trunc(f*pow+math.Copysign(0.5, f)) / pow
+}
