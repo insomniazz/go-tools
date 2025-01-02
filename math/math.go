eecc12c8605d4c48e1d48a7e0aeab8476559c850
chen <test@test.com>
2025-01-02 20:28:27 +0800

    save

--- math/math.go
+++ math/math.go
@@ -1,6 +1,9 @@
 package math
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func MyRoundFloat(val float64, precision int8) float64 {
 	ratio := math.Pow10(int(precision))
@@ -11,6 +14,7 @@ func MyRoundFloat2(f float64, digits int) float64 {
 	//if math.Abs(f) < 0.5 {
 	//	return 0
 	//}
+	fmt.Println("v1.1.1")
 	pow := math.Pow10(digits)
 	return math.Trunc(f*pow+math.Copysign(0.5, f)) / pow
 }
